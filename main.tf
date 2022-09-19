cebaee09817e8f97831592b3971907fb12b1b7ea
devdubu <insomnis2020@naver.com>
2022-09-19 10:24:23 +0900

    complete part1

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     local = {
-      source = "hashicorp/local"
+      source  = "hashicorp/local"
       version = "2.2.3"
     }
   }
@@ -15,20 +15,20 @@ provider "awscc" {
 }
 
 resource "aws_vpc" "foo" {
-    cidr_block = "10.123.0.0/16"
+  cidr_block = "10.123.0.0/16"
 
-    tags = {
-        "Name" = "This is test vpc"
-    }
+  tags = {
+    "Name" = "This is test vpc"
+  }
 }
 
 resource "local_file" "foo" {
-    filename = "${path.module}/foo.txt"
-    content  = "Hello World!"
+  filename = "${path.module}/foo.txt"
+  content  = "Hello World!"
 }
 
 data "local_file" "foo" {
-    filename = "${path.module}/foo.txt"
+  filename = "${path.module}/foo.txt"
 }
 
 output "vpc_foo" {
@@ -39,9 +39,9 @@ output "file_foo" {
   value = data.local_file.foo
 }
 
-data "aws_vpcs" "this"{ 
+data "aws_vpcs" "this" {
 }
 
 output "vpcs" {
-   value = data.aws_vpcs.this
+  value = data.aws_vpcs.this
 }
